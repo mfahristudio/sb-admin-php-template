ebd49f1e9975ac9823d0787a7ce11521822d73b1
M.Fahri Studio <fahri.mohammad@gmail.com>
2026-01-09 08:46:26 +0800

    Update validasi-login.php

--- validasi-login.php
+++ validasi-login.php
@@ -1,22 +1,26 @@
 <?php
 error_reporting(0);
-function valiasiLogin() {
-    session_start();
-    if (!isset($_SESSION['user_id']) || !isset($_SESSION['username'])) {
-        // Hapus cookies user_id dan username
-        setcookie('user_id', '', time() - 3600, '/');
-        setcookie('username', '', time() - 3600, '/');
+
+function validateLogin() {
+    if (session_status() === PHP_SESSION_NONE) {
+        session_start();
+    }
+    
+    if (empty($_SESSION['user_id']) || empty($_SESSION['username'])) {
+        $cookies = ['user_id', 'username'];
         
-        // Akhiri session
+        foreach ($cookies as $cookie) {
+            if (isset($_COOKIE[$cookie])) {
+                setcookie($cookie, '', time() - 3600, '/');
+            }
+        }
+        
         session_destroy();
-        
-        // Redirect ke halaman login
         header('Location: index.php');
         exit();
-    } else {
-        // User valid, require_once core.php
-        require_once "core.php";
     }
+    
+    require_once "core.php";
 }
-valiasiLogin();
-?>
+
+validateLogin();
